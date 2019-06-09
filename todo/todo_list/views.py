6a184a4b39cb6f9f6617709a9ca36af4330a99ac
amirdeve <amirdeveloper92@gmail.com>
2019-06-09 14:48:15 +0430

    create form and set POST method action

--- todo/todo_list/views.py
+++ todo/todo_list/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import list
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
-    return render(request, "home.html",{})
+    if request.method == 'POST':
+        form=ListForm(request.POST or None)
 
-# Create your views here.
+        if form.is_valid():
+            form.save()
+            all_items=list.objects.all
+            messages.success(request, ('Items has been added to list!'))
+            return render(request, 'home.html', {'all_items':all_items})
+
+    else:
+        all_items=list.objects.all
+        return render(request, 'home.html', {'all_items':all_items})
